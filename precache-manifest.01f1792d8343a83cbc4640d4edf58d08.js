d6d21b20243c12e24ca4a698961d9c074de6a9b0
Park Gyu Ae <yunos0502@gmail.com>
2020-08-27 00:37:27 +0900

    Updates

---
+++ precache-manifest.01f1792d8343a83cbc4640d4edf58d08.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa1a76e0ea651ca338ee88d1e22fc9df",
+    "url": "/movie_app_2020/index.html"
+  },
+  {
+    "revision": "4ea5c781b49de1544692",
+    "url": "/movie_app_2020/static/css/main.f59aa2d4.chunk.css"
+  },
+  {
+    "revision": "d10d8ce8ba9e24f1a4f1",
+    "url": "/movie_app_2020/static/js/2.807c5a94.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/movie_app_2020/static/js/2.807c5a94.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4ea5c781b49de1544692",
+    "url": "/movie_app_2020/static/js/main.03a30211.chunk.js"
+  },
+  {
+    "revision": "6636fcdddd453d60abc3",
+    "url": "/movie_app_2020/static/js/runtime-main.9a083715.js"
+  }
+]);
